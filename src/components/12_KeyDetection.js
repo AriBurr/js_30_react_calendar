3caaa7ce82e351e796ec355a7ef444f851fc0bbb
Ariel Burriss <ariburr@gmail.com>
2018-03-25 11:35:12 -0600

    reset input after delay

--- src/components/12_KeyDetection.js
+++ src/components/12_KeyDetection.js
@@ -2,25 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 class KeyDetection extends React.Component {
-  state = { secretCode: 'abc', pressed: [], win: false };
+  state = { secretCode: 'abc', pressed: [], win: false, input: '' };
 
   componentDidUpdate() {
     const { secretCode, pressed, win } = this.state;
-    if (!win && pressed.join('').includes(secretCode))
+    if (!win && pressed.join('').includes(secretCode)) {
       this.setState({ win: true });
+      setTimeout(() => {
+        this.setState({ win: false, pressed: [], input: '' });
+      }, 5000);
+    }
   }
 
   handleChange = e => {
-    const { pressed } = this.state;
-    this.setState({ pressed: [...pressed, e.key] });
+    const { pressed, input } = this.state;
+    this.setState({ pressed: [...pressed, e.key], input: input + e.key });
   };
 
   render() {
-    const { win } = this.state;
+    const { win, input } = this.state;
     return (
       <Container>
         <span>
-          <input onKeyUp={this.handleChange} />
+          <input value={input} onKeyUp={this.handleChange} />
           <div>{win && 'DING DING DING!!!'}</div>
         </span>
       </Container>
