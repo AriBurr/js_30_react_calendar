b94e34d417f7402b3e93d7d86a8d62d5a44f399f
Ariel Burriss <ariburr@gmail.com>
2018-03-27 20:55:50 -0600

    add images to slide

--- src/components/13_Slide.js
+++ src/components/13_Slide.js
@@ -4,10 +4,14 @@ import '../styles/slide.css';
 class Slide extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <div className='site-wrap'>
+      <div className="container">
+        <div className="site-wrap">
           <h1>Slide in on Scroll</h1>
-          <img src='' className='align-left slide-in' alt='placeholder' />
+          <img
+            src="http://unsplash.it/400/400"
+            className="align-left slide-in"
+            alt="placeholder"
+          />
           <p>
             Consectetur adipisicing elit. Tempore tempora rerum, est autem
             cupiditate, corporis a qui libero ipsum delectus quidem dolor at
@@ -15,14 +19,22 @@ class Slide extends React.Component {
             quod quas laborum nam! Fuga ad tempora in aspernatur pariaturlores
             sunt esse magni, ut, dignissimos.
           </p>
-          <img src='' className='align-right slide-in' alt='placeholder' />
+          <img
+            src="http://unsplash.it/400/401"
+            className="align-right slide-in"
+            alt="placeholder"
+          />
           <p>
             Lorem ipsum cupiditate, corporis a qui libero ipsum delectus quidem
             dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis
             blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur
             pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.
           </p>
-          <img src='' className='align-left slide-in' alt='placeholder' />
+          <img
+            src="http://unsplash.it/200/500"
+            className="align-left slide-in"
+            alt="placeholder"
+          />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
             tempora rerum, est autem cupiditate, corporis a qui libero ipsum
@@ -31,7 +43,11 @@ class Slide extends React.Component {
             in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut,
             dignissimos.
           </p>
-          <img src='' className='align-right slide-in' alt='placeholder' />
+          <img
+            src="http://unsplash.it/400/400"
+            className="align-right slide-in"
+            alt="placeholder"
+          />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
             tempora rerum, est autem cupiditate, corporis a qui libero ipsum
@@ -47,11 +63,6 @@ class Slide extends React.Component {
 }
 
 export default Slide;
-
-// http://unsplash.it/400/400
-// http://unsplash.it/400/401
-// http://unsplash.it/200/500
-// http://unsplash.it/400/400
 
 //   <script>
 //     function debounce(func, wait = 20, immediate = true) {
